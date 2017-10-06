5b59bcb9eb1ba045ba2afab0e1a229c9f8b0364c
Justin Daniel <justinddaniel@gmail.com>
2017-10-06 18:40:42 +0000

    Deleted My_Pet file as it was not needed as anticipated.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,6 @@
 require "bundler/setup"
 require "Adopt_with_Love"
 require "open-uri"
-require "nokogiri" #needs to be installed with 'gem install nokogiri'
+require "nokogiri"
 require_relative "../lib/AwL_controller.rb"
-require_relative "../lib/My_Pet.rb"
 require_relative "../lib/Pets_Listing.rb"
--- lib/AwL_controller.rb
+++ lib/AwL_controller.rb
@@ -40,8 +40,4 @@ class AwLController
     peturl = self.pet.petprofiles[index]
     puts peturl
   end
-
-
-
-
 end
