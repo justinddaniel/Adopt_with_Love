7f35c0dfff21aa46368c1dff97d0e7e65892ad4a
Justin Daniel <justinddaniel@gmail.com>
2017-10-07 21:36:59 +0000

    More comments added explaining the code. This is final pre-assessment version.

--- lib/AwL_controller.rb
+++ lib/AwL_controller.rb
@@ -12,7 +12,7 @@ class AwLController
     puts "If you are intested in adopting a cat, type '2'."
     petchoice #new instance method that will take user input and call the scraper for the appropriate website.
     puts "please type the number of the pet you wish to view, or type 'exit'"
-    petselection
+    petselection #This instance method takes the user's next input to return a weblink to the pet's profile.
   end
 
   def petchoice
@@ -27,22 +27,26 @@ class AwLController
       self.pet = PetsListing.new
       self.pet.cat
     else
-      puts "Invalid selection. Please type '1' or '2'."
+      puts "Invalid selection. Please type '1' or '2'." #This line is necessary in case a user types something other than an integer of 1 or 2.
+      #It is designed to display the error message. The line below then loops the user back to the beginning.
       self.summon
     end
   end
 
   def petselection
     input = gets.strip
-    if input == 'exit'
+    if input == 'exit' #Users probably will want a way to get out of this sectin and return to where they can choose a different pet to look at.
       self.summon
-    elsif  input.to_i > 0
-      index = (input.to_i - 1)
+    elsif  input.to_i > 0 #I found it necessary to specify that input.to_i method makes an integer of greater than 1 because I found that if text
+      #such as "pet" is entered by the user it will convert it to an integer of 0! I thus needed to give the program a way of taking such input and
+      #returning an error message and looping them back to the start of the selection.
+      index = (input.to_i - 1) #The code pulls the weblink from an array and thus the input number needs to be subtracted by one for the index of the array.
     else
       puts "Invalid selection. Please type the number of the pet you wish to view or type 'exit'."
       petselection
     end
     peturl = self.pet.petprofiles[index]
-    puts peturl
+    puts peturl #This is the end of the interaction where the user can simply click on the weblink the interface puts out to go directly to the
+    #pets profile page in the browser.
   end
 end
--- lib/Pets_Listing.rb
+++ lib/Pets_Listing.rb
@@ -3,32 +3,36 @@ require 'nokogiri'
 class PetsListing
 
   attr_accessor :dog, :cat
-  attr_reader :petslist, :petprofiles, :AwLController
+  attr_reader :petslist, :petprofiles, :AwLController #I made these readers and not also writers to help discourage abuse of the instance
+  #variables petslist and petprofiles, as well as the AwLController class. If a programmer pushes strings rather than class/instance objects
+  # to these, for example, the code will break.
 
   def initialize
-    @petslist = []
+    @petslist = [] #Once a new class object is instantiated from the AwL_controller then it needs two empty array for objects to be pushed into later.
     @petprofiles = []
   end
 
   def dog
     html = open("http://humanesocietyswm.org/dogs-for-adoption/")
-    doc = Nokogiri::HTML(html)
-    sites = doc.css("#grid-gallery-3 a")
+    doc = Nokogiri::HTML(html) #This is the line that allows the nokogiri gem to start scraping from the website (html)
+    sites = doc.css("#grid-gallery-3 a") #This selects the individual dog objects which include names and profile links.
     sites.each do |s|
-      @petprofiles << s['href']
+      @petprofiles << s['href'] #This accesses the weblinks of each animal and saves them for later as separate indexes in the petprofiles array.
     end
 
     names = doc.css(".gg-image-caption")
     c = 1
     names.each do |n|
-      @petslist << "#{c}. #{n.text}"
+      @petslist << "#{c}. #{n.text}" #This saves the name of each animal to the petslist.
       c += 1
     end
-    puts @petslist
+    puts @petslist #This returns the petlist by printing out one line for each name; this makes it easier for the user to reader
+    #rather than having to scroll through an array. The user then can select the number of he name of the animal they wish to view (coded elsewhere).
   end
 
   def cat
-    html = open("http://humanesocietyswm.org/cats-for-adoption/")
+    html = open("http://humanesocietyswm.org/cats-for-adoption/") #The notes above all apply for the cat method as well; they are virtually identical
+    # to the dog method except a different webpage and slightly different css selectors were needed.
     doc = Nokogiri::HTML(html)
     sites = doc.css("#grid-gallery-1 a")
     sites.each do |s|
