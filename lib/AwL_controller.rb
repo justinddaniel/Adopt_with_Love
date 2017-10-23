e0c80ce096f1191c5338e583faa83dcaf4245616
Justin Daniel <justinddaniel@gmail.com>
2017-10-23 22:12:42 +0000

    refactoring with help from Cernan Bernardo

--- lib/AwL_controller.rb
+++ lib/AwL_controller.rb
@@ -19,10 +19,9 @@ class AwLController
     input = gets.strip
     input = input.to_i
     if input == 1
-      self.pet = PetsListing.new #This line instantiates a new PetsListing object and ensures communication
-      #between the two objects by making the pet instance variable of self a new instance of the PetsListing Class.
-      self.pet.dog #calls the instance method of dog, which is part of the PetsListing Class. This will actually do the scraping from the dog
+      PetsListing.dog #calls the instance method of dog, which is part of the PetsListing Class. This will actually do the scraping from the dog
       #section of the website.
+      list_dogs
     elsif input == 2  #The next 3 lines of code are mirror images of above in the case users want to look at cats.
       self.pet = PetsListing.new
       self.pet.cat
@@ -33,11 +32,17 @@ class AwLController
     end
   end
 
+  def list_dogs
+    PetsListing.all.each.with_index(1) do |p, index|
+      puts "#{index}. #{p.name}"
+    end
+  end
+
   def petselection
     input = gets.strip
     if input == 'exit' #Users probably will want a way to get out of this sectin and return to where they can choose a different pet to look at.
       self.summon
-    elsif  input.to_i > 0 #I found it necessary to specify that input.to_i method makes an integer of greater than 1 because I found that if text
+    elsif  input.to_i.between?(1, PetsListing.all.size) #I found it necessary to specify that input.to_i method makes an integer of greater than 1 because I found that if text
       #such as "pet" is entered by the user it will convert it to an integer of 0! I thus needed to give the program a way of taking such input and
       #returning an error message and looping them back to the start of the selection.
       index = (input.to_i - 1) #The code pulls the weblink from an array and thus the input number needs to be subtracted by one for the index of the array.
@@ -45,8 +50,8 @@ class AwLController
       puts "Invalid selection. Please type the number of the pet you wish to view or type 'exit'."
       petselection
     end
-    peturl = self.pet.petprofiles[index]
-    puts peturl #This is the end of the interaction where the user can simply click on the weblink the interface puts out to go directly to the
+    pet = PetsListing.all[index]
+    puts pet.url #This is the end of the interaction where the user can simply click on the weblink the interface puts out to go directly to the
     #pets profile page in the browser.
   end
 end
--- lib/Pets_Listing.rb
+++ lib/Pets_Listing.rb
@@ -2,32 +2,37 @@ require 'pry'
 require 'nokogiri'
 class PetsListing
 
-  attr_accessor :dog, :cat
+  attr_accessor :name, :url, :type
   attr_reader :petslist, :petprofiles, :AwLController #I made these readers and not also writers to help discourage abuse of the instance
   #variables petslist and petprofiles, as well as the AwLController class. If a programmer pushes strings rather than class/instance objects
   # to these, for example, the code will break.
-
-  def initialize
-    @petslist = [] #Once a new class object is instantiated from the AwL_controller then it needs two empty array for objects to be pushed into later.
-    @petprofiles = []
+  @@all = []
+  def initialize(name, url, type)
+    @name = name
+    @url = url
+    @type = type
+    @@all << self
   end
 
-  def dog
+  def self.dog
     html = open("http://humanesocietyswm.org/dogs-for-adoption/")
     doc = Nokogiri::HTML(html) #This is the line that allows the nokogiri gem to start scraping from the website (html)
-    sites = doc.css("#grid-gallery-3 a") #This selects the individual dog objects which include names and profile links.
-    sites.each do |s|
-      @petprofiles << s['href'] #This accesses the weblinks of each animal and saves them for later as separate indexes in the petprofiles array.
+    #binding.pry
+    doc.css('figure').each do |f|
+      url = f.parent.attr("href")
+      name = f.css(".gg-image-caption").text.strip
+      self.new(name, url, 'dog')
     end
+  end
 
-    names = doc.css(".gg-image-caption")
-    c = 1
-    names.each do |n|
-      @petslist << "#{c}. #{n.text}" #This saves the name of each animal to the petslist.
-      c += 1
-    end
-    puts @petslist #This returns the petlist by printing out one line for each name; this makes it easier for the user to reader
-    #rather than having to scroll through an array. The user then can select the number of he name of the animal they wish to view (coded elsewhere).
+  def self.dogs
+
+    all.select{|p| p.type == 'dog'}
+  end
+
+  def self.all
+
+    @@all
   end
 
   def cat
